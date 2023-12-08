53d44e0a9445cf9a37fd05daf2c7f9abe80f0224
Todor Garnev <tgarnev@abv.bg>
2023-12-08 17:28:03 +0200

    added styles; added getting 10 pokemons in home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { For, createEffect, createSignal, onMount } from "solid-js";
+import styles from "./App.module.css";
+
 export const App = () => {
+  const [pokemons, setPokemons] = createSignal<Record<string, string>[]>([]);
+
+  onMount(async () => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
+    setPokemons(await res.json().then((data) => data.results));
+  });
+
+  createEffect(() => console.log(pokemons()));
+
   return (
     <>
-      <h1>Pokedex</h1>
+      <header class={styles.header}>
+        <h1>Pokedex</h1>
+      </header>
+
+      <main>
+        <For each={pokemons()}>
+          {(pokemon, i) => (
+            <div class={styles.pokemon}>
+              <span class={styles.pokemonIndex}>{i() + 1}.</span>
+              <span class={styles.pokemonName}>{pokemon.name}</span>
+              <button>More info</button>
+            </div>
+          )}
+        </For>
+      </main>
     </>
   );
 };
